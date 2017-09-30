8c56f346a1075b4b7a9d54d3c2e17ccb13bc2524
Saif Khan <saifkhichi96@gmail.com>
2017-10-01 02:52:32 +0500

    Created splash screen

---
+++ app/src/main/java/pk/edu/seecs/cs361/paint/MainActivity.java
@@ -0,0 +1,13 @@
+package pk.edu.seecs.cs361.paint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
