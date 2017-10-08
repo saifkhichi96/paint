587609901aac7982b25fb56aa14352f28035b8ab
Saif Khan <saifkhichi96@gmail.com>
2017-10-08 19:08:25 +0500

    Merged Ali's pen code

---
+++ app/src/main/java/pk/edu/seecs/cs361/paint/PaintView.java
@@ -0,0 +1,138 @@
+package pk.edu.seecs.cs361.paint;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+
+
+public class PaintView extends View {
+
+    public static int BRUSH_SIZE = 10;
+    public static final int DEFAULT_COLOR = Color.YELLOW;
+    public static final int DEFAULT_BG_COLOR = Color.BLACK;
+    private static final float TOUCH_TOLERANCE = 10;
+
+    private float mX, mY;
+    private Path mPath;
+    private Paint mPaint;
+    private ArrayList<FingerPath> paths = new ArrayList<>();
+    private int currentColor;
+    private int backgroundColor = DEFAULT_BG_COLOR;
+    private int strokeWidth;
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+    private Paint mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+
+    public PaintView(Context context) {
+        this(context, null);
+    }
+
+    public PaintView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPaint = new Paint();
+
+//        mPaint.setAntiAlias(true);
+//        mPaint.setDither(true);
+
+        mPaint.setColor(DEFAULT_COLOR);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.MITER);
+        mPaint.setStrokeCap(Paint.Cap.BUTT);
+        mPaint.setXfermode(null);
+        mPaint.setAlpha(0xff);
+
+    }
+
+    public void init(DisplayMetrics metrics) {
+        int height = metrics.heightPixels;
+        int width = metrics.widthPixels;
+
+        mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+
+        currentColor = DEFAULT_COLOR;
+        strokeWidth = BRUSH_SIZE;
+    }
+
+    public void clear() {
+        backgroundColor = DEFAULT_BG_COLOR;
+        paths.clear();
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.save();
+        mCanvas.drawColor(backgroundColor);
+
+        for (FingerPath fp : paths) {
+            mPaint.setColor(fp.color);
+            mPaint.setStrokeWidth(fp.strokeWidth);
+//            mPaint.setMaskFilter(null);
+
+            mCanvas.drawPath(fp.path, mPaint);
+
+        }
+
+        canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+        canvas.restore();
+    }
+
+    private void touchStart(float x, float y) {
+        mPath = new Path();
+        FingerPath fp = new FingerPath(currentColor, strokeWidth, mPath);
+        paths.add(fp);
+
+        mPath.reset();
+        mPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+
+    private void touchMove(float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+
+        if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+            mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+            mX = x;
+            mY = y;
+        }
+    }
+
+    private void touchUp() {
+        mPath.lineTo(mX, mY);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN :
+                touchStart(x, y);
+//                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE :
+                touchMove(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP :
+                touchUp();
+                invalidate();
+                break;
+        }
+
+        return true;
+    }
+}
