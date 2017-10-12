6b2266c0e407c1b025c6b7bde2cb0153a73ef55b
Saif Khan <saifkhichi96@gmail.com>
2017-10-12 07:28:36 +0500

    Refactored action bar and toolbox code into separate classes.

--- app/src/main/java/pk/edu/seecs/cs361/paint/MainActivity.java
+++ app/src/main/java/pk/edu/seecs/cs361/paint/MainActivity.java
@@ -10,73 +10,81 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 
 import pk.edu.seecs.cs361.paint.core.PaintCanvas;
+import pk.edu.seecs.cs361.paint.utils.ActionBarManager;
+import pk.edu.seecs.cs361.paint.view.ToolboxView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private LinearLayout toolbar;
-    private int selectedTool = -1;
-
+    // A custom OpenGL ES canvas to draw on
     private PaintCanvas paintCanvas;
 
-    private AlertDialog confirmClear;
-    private AlertDialog confirmSave;
+    // Toolbox contains the drawing tools
+    private ToolboxView toolbox;
+
+    // Dialog boxes to confirm certain permanent actions (i.e. revert and save)
+    private AlertDialog revertConfirmation;
+    private AlertDialog saveConfirmation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setSupportActionBar((Toolbar) findViewById(R.id.actionBar));
+        getSupportActionBar().setTitle("");
+
         // Create confirmation dailogs
-        confirmClear = new AlertDialog.Builder(this)
+        revertConfirmation = new AlertDialog.Builder(this)
                 .setTitle("Are you sure?")
                 .setMessage("This action will erase everything drawn on canvas. It cannot be reversed. Do you really wish to proceed?")
                 .setPositiveButton("YES", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         paintCanvas.clear();
-                        confirmClear.dismiss();
+                        revertConfirmation.dismiss();
                     }
                 })
                 .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        confirmClear.dismiss();
+                        revertConfirmation.dismiss();
                     }
                 })
                 .create();
 
-        confirmSave = new AlertDialog.Builder(this)
+        saveConfirmation = new AlertDialog.Builder(this)
                 .setTitle("Save doodle to Galley?")
                 .setPositiveButton("YES", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         paintCanvas.save();
-                        confirmClear.dismiss();
+                        revertConfirmation.dismiss();
                         finish();
                     }
                 })
                 .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        confirmClear.dismiss();
+                        revertConfirmation.dismiss();
                     }
                 })
                 .create();
 
-        // Add click event listeners to toolbar buttons
-        toolbar = (LinearLayout) findViewById(R.id.toolbar);
-        for (int i = 0; i < toolbar.getChildCount(); i++) {
-            toolbar.getChildAt(i).setOnClickListener(this);
-        }
-        findViewById(R.id.clear).setOnClickListener(this);
-        findViewById(R.id.undo).setOnClickListener(this);
-        findViewById(R.id.redo).setOnClickListener(this);
+        // Add click event listeners to toolbox buttons
+        toolbox = (ToolboxView) findViewById(R.id.toolbox);
+        toolbox.addUnselectable(4);
+        toolbox.addUnselectable(5);
+        toolbox.setOnClickListener(this);
+
         findViewById(R.id.save).setOnClickListener(this);
 
         // Initialize canvas where everything is drawn
@@ -95,7 +103,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent data = getIntent().getParcelableExtra("FROM_GALLERY");
         if (data != null) {
             Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
 
             Cursor cursor = getContentResolver().query(selectedImage,
                     filePathColumn, null, null, null);
@@ -112,47 +120,69 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         // Select Pen tool by default
-        enableButton(0);
+        toolbox.selectTool(0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+
+        ActionBarManager actionBarManager = new ActionBarManager(menu);
+        paintCanvas.setCanvasActionListener(actionBarManager);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.undo:
+                paintCanvas.undo();
+                return true;
+
+            case R.id.redo:
+                paintCanvas.redo();
+                return true;
+
+            case R.id.revert:
+                revertConfirmation.show();
+                return true;
+
+        }
+
+        return false;
     }
 
     @Override
     public void onClick(View v) {
-        for (int i = 0; i < toolbar.getChildCount(); i++) {
-            if (v.getId() == toolbar.getChildAt(i).getId()) {
+        for (int i = 0; i < toolbox.getChildCount(); i++) {
+            if (v.getId() == toolbox.getChildAt(i).getId()) {
                 switch (i) {
                     case 0: // PEN
-                        disableButtons();
-                        enableButton(i);
                         paintCanvas.enablePen();
                         break;
                     case 1: // LINE
-                        disableButtons();
-                        enableButton(i);
                         paintCanvas.enableLine();
                         break;
                     case 2: // BOX
-                        if (selectedTool == 2) {
+                        if (toolbox.getSelectedTool() == 2) {
                             if (paintCanvas.toggle3D()) {
-                                ((ImageButton) toolbar.getChildAt(i)).setImageResource(R.drawable.ic_box);
+                                ((ImageButton) toolbox.getChildAt(i)).setImageResource(R.drawable.ic_box);
                             } else {
-                                ((ImageButton) toolbar.getChildAt(i)).setImageResource(R.drawable.ic_square);
+                                ((ImageButton) toolbox.getChildAt(i)).setImageResource(R.drawable.ic_square);
                             }
                         }
 
-                        disableButtons();
-                        enableButton(i);
                         paintCanvas.enableBox();
                         break;
                     case 3: // CIRCLE
-                        disableButtons();
-                        enableButton(i);
                         paintCanvas.enableCircle();
                         break;
                     case 4: // STROKE WIDTH
                         if (paintCanvas.toggleFilled()) {
-                            ((ImageButton) toolbar.getChildAt(i)).setImageResource(R.drawable.ic_fill);
+                            ((ImageButton) toolbox.getChildAt(i)).setImageResource(R.drawable.ic_fill);
                         } else {
-                            ((ImageButton) toolbar.getChildAt(i)).setImageResource(R.drawable.ic_fill_none);
+                            ((ImageButton) toolbox.getChildAt(i)).setImageResource(R.drawable.ic_fill_none);
                         }
                         break;
                     case 5: // COLOR PICKER
@@ -160,53 +190,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         // Display color picker here. And set color using paintCanvas.setBrushColor once a color is picked.
                         break;
                 }
-                selectedTool = i;
             }
         }
 
         switch (v.getId()) {
-            case R.id.clear:
-                confirmClear.show();
-                break;
-
-            case R.id.undo:
-                paintCanvas.undo();
-                if (paintCanvas.canRedo()) {
-                    findViewById(R.id.redo).setVisibility(View.VISIBLE);
-                }
-                break;
-
-            case R.id.redo:
-                paintCanvas.redo();
-                if (!paintCanvas.canRedo()) {
-                    findViewById(R.id.redo).setVisibility(View.GONE);
-                }
-                break;
-
             case R.id.save:
-                confirmSave.show();
+                saveConfirmation.show();
                 break;
         }
 
-    }
-
-    private void setToolbarItemColor(int itemIndex, int color) {
-        try {
-            ((ImageButton) toolbar.getChildAt(itemIndex)).setColorFilter(color);
-        } catch (Exception ignored) {
-
-        }
-    }
-
-    private void enableButton(int toolId) {
-        setToolbarItemColor(toolId, Color.parseColor("#883997"));
-    }
-
-    private void disableButtons() {
-        for (int i = 0; i < toolbar.getChildCount(); i++) {
-            if (i == 4) continue;
-            setToolbarItemColor(i, Color.WHITE);
-        }
     }
 
 }
