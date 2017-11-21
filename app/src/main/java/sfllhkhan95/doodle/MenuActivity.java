0709d894b08f72bcbb234d153a0e1da599f7cd1e
Muhammad Saifullah Khan <sfllhkhan95@live.com>
2017-11-21 15:18:29 +0500

    Bug fixes and redesigned homepage

--- app/src/main/java/sfllhkhan95/doodle/MenuActivity.java
+++ app/src/main/java/sfllhkhan95/doodle/MenuActivity.java
@@ -1,74 +1,78 @@
 package sfllhkhan95.doodle;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.annotation.UiThread;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import sfllhkhan95.doodle.models.Thumbnail;
 import sfllhkhan95.doodle.utils.DoodleDatabase;
+import sfllhkhan95.doodle.utils.ThumbnailAdapter;
 
-public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+public class MenuActivity extends AppCompatActivity {
 
     private static final int RESULT_LOAD_IMAGE = 200;
+
+    private final ProjectScanner scanner = new ProjectScanner();
+    private final ThumbnailInflater inflater = new ThumbnailInflater();
+    private String[] savedProjects;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        findViewById(R.id.whiteDoodle).setOnClickListener(this);
-        findViewById(R.id.blackDoodle).setOnClickListener(this);
-        findViewById(R.id.fromImage).setOnClickListener(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("HOME");
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.menuActivity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("BG_COLOR", Color.BLACK);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        String[] savedDoodles = DoodleDatabase.listDoodles();
-        if (savedDoodles != null && savedDoodles.length > 0) {
-            LinearLayout doodles = (LinearLayout) findViewById(R.id.savedDoodles);
-            doodles.removeAllViews();
-
-            for (final String doodle : savedDoodles) {
-                View.inflate(this, R.layout.saved_doodle, doodles);
-                View view = doodles.getChildAt(doodles.getChildCount() - 1);
-
-                TextView title = view.findViewById(R.id.doodleTitle);
-                title.setText(doodle);
-
-                ImageView thumbnail = view.findViewById(R.id.doodleThumbnail);
-                thumbnail.setImageBitmap(DoodleDatabase.loadDoodle(doodle, 100, 100));
-
-                view.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                        intent.putExtra("DOODLE", doodle);
-                        startActivity(intent);
-                    }
-                });
-            }
-        }
+        scanner.start();
     }
 
     @Override
-    public void onClick(View v) {
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        switch (v.getId()) {
-            case R.id.whiteDoodle:
-                intent.putExtra("BG_COLOR", Color.WHITE);
-                startActivity(intent);
-                break;
+    protected void onPause() {
+        super.onPause();
+        scanner.finish();
+    }
 
-            case R.id.blackDoodle:
-                intent.putExtra("BG_COLOR", Color.BLACK);
-                startActivity(intent);
-                break;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
             case R.id.fromImage:
                 Intent i = new Intent(
                         Intent.ACTION_PICK,
@@ -77,6 +81,8 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
                 startActivityForResult(i, RESULT_LOAD_IMAGE);
                 break;
         }
+
+        return false;
     }
 
     @Override
@@ -91,4 +97,93 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
 
 
     }
+
+    @UiThread
+    private class ThumbnailInflater implements Runnable,
+            AdapterView.OnItemClickListener {
+
+        @Nullable
+        private List<Thumbnail> getThumbnails() {
+            List<Thumbnail> thumbnails = null;
+            if (savedProjects != null && savedProjects.length > 0) {
+                thumbnails = new ArrayList<>();
+
+                Bitmap icon = BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_add);
+                thumbnails.add(new Thumbnail(icon, "NEW PROJECT"));
+
+                for (String projectName : savedProjects) {
+                    Bitmap thumbnailBitmap;
+                    if ((thumbnailBitmap = DoodleDatabase.loadDoodle(projectName, 100, 100)) != null) {
+                        Thumbnail thumbnail = new Thumbnail(thumbnailBitmap, projectName);
+                        thumbnails.add(thumbnail);
+                    }
+                }
+            }
+            return thumbnails;
+        }
+
+        @Override
+        public void run() {
+            List<Thumbnail> thumbnails = getThumbnails();
+            if (thumbnails == null) {
+                findViewById(R.id.savedDoodles).setVisibility(View.GONE);
+                findViewById(R.id.tapAnywhere).setVisibility(View.VISIBLE);
+            } else {
+                findViewById(R.id.savedDoodles).setVisibility(View.VISIBLE);
+                findViewById(R.id.tapAnywhere).setVisibility(View.GONE);
+
+                ThumbnailAdapter adapter = new ThumbnailAdapter(
+                        MenuActivity.this,
+                        R.layout.template_thumbnail,
+                        thumbnails);
+
+                GridView doodlesView = (GridView) findViewById(R.id.savedDoodles);
+                doodlesView.setAdapter(adapter);
+
+                doodlesView.setOnItemClickListener(this);
+            }
+        }
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            if (position == 0) {
+                intent.putExtra("BG_COLOR", Color.BLACK);
+            } else {
+                Thumbnail item = (Thumbnail) parent.getItemAtPosition(position);
+                intent.putExtra("DOODLE", item.getName());
+
+                if (!DoodleDatabase.contains(item.getName())) return;
+            }
+            startActivity(intent);
+        }
+
+    }
+
+    private class ProjectScanner extends Thread {
+
+        private Handler mHandler = new Handler();
+        private boolean isListening = true;
+
+        @Override
+        public void run() {
+            savedProjects = DoodleDatabase.listDoodles();
+            runOnUiThread(inflater);
+
+            if (isListening) {
+                mHandler.postDelayed(this, 100);
+            }
+        }
+
+        @Override
+        public synchronized void start() {
+            mHandler.post(this);
+        }
+
+        public void finish() {
+            isListening = false;
+        }
+
+    }
+
 }
