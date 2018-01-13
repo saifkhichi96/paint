637cbf3c42f97ad97871358cd4c9bd9c04b7a3be
Muhammad Saifullah Khan <sfllhkhan95@live.com>
2018-01-14 04:09:59 +0500

    Added Facebook SDK

---
+++ app/src/main/java/sfllhkhan95/doodle/Doodle.java
@@ -0,0 +1,45 @@
+package sfllhkhan95.doodle;
+
+import android.app.Application;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+/**
+ * Doodle is the Application class which bootstraps everything and initializes the global
+ * state of the app.
+ *
+ * @author saifkhichi96
+ * @version 1.0
+ */
+public class Doodle extends Application {
+
+    /**
+     * Called when the application is starting, before any other application objects
+     * are created. Used for initial configuration.
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        /* The following method call initializes the Facebook SDK, and is recommended to
+         * be called as early as possible. The behavior of Facebook SDK functions is
+         * undetermined if this function is not called.
+         *
+         * UPDATE SDK v4.19+: The Facebook SDK is now auto initialized on Application start. If you are
+         * using the Facebook SDK in the main process and don't need a callback on SDK
+         * initialization completion you can now remove calls to FacebookSDK.sdkInitialize.
+         * If you do need a callback, you should manually invoke the callback in your code.
+         * (i.e. the following line of code can be safely removed.) */
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+
+        /* Activating Facebook SDK's app event logging is required for the app to be
+         * eligible for Facebook's App Review submission.
+         *
+         * UPDATE SDK v4.19+: It is automatically initialized unless disabled.
+         * (i.e. the following line of code can be safely removed.) */
+        AppEventsLogger.activateApp(this);
+    }
+
+}
