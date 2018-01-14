fe32dfd4321971dcf3cf24193ac14a6c0ce034b1
Saif Khan <saifkhichi96@gmail.com>
2018-01-14 06:13:49 +0500

    Fixed eraser color sync issue with canvas color

--- app/src/main/java/sfllhkhan95/doodle/shapes/Pen.java
+++ app/src/main/java/sfllhkhan95/doodle/shapes/Pen.java
@@ -1,10 +1,10 @@
 package sfllhkhan95.doodle.shapes;
 
-import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PointF;
 
 import sfllhkhan95.doodle.core.PaintBrush;
+import sfllhkhan95.doodle.core.PaintCanvas;
 
 /**
  * Created by saifkhichi96 on 23/10/2017.
@@ -16,7 +16,7 @@ public class Pen extends Shape {
     }
 
     @Override
-    public void paint(Canvas canvas) {
+    public void paint(PaintCanvas canvas) {
         paintBrush.setStyle(Paint.Style.STROKE);
         paintBrush.setColor(paintBrush.getStrokeColor());
         paintBrush.setStrokeWidth(paintBrush.getSize());
