517cdef927db8786cb846d65f6f8578799395241
Saif Khan <saifkhichi96@gmail.com>
2018-06-04 00:10:06 +0500

    UPDATE: Options dialog UI updated. Support dialog added.

--- app/src/main/java/sfllhkhan95/doodle/core/utils/DialogFactory.java
+++ app/src/main/java/sfllhkhan95/doodle/core/utils/DialogFactory.java
@@ -3,12 +3,15 @@ package sfllhkhan95.doodle.core.utils;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.DrawableRes;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
@@ -17,6 +20,7 @@ import com.google.android.gms.ads.MobileAds;
 
 import sfllhkhan95.doodle.R;
 import sfllhkhan95.doodle.core.MainActivity;
+import sfllhkhan95.doodle.core.views.OptionsDialog;
 import sfllhkhan95.doodle.core.views.PaintView;
 
 public class DialogFactory {
@@ -46,47 +50,54 @@ public class DialogFactory {
     }
 
     public Dialog shareDialog(final MainActivity context) {
-        return new Dialog(context) {
-            @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.dialog_share);
-
-                // Set click listeners
-                findViewById(R.id.messengerShareButton).setOnClickListener(new View.OnClickListener() {
+        return new OptionsDialog.Builder(context)
+                .setIcon(R.drawable.ic_shape)
+                .setTitle("Share")
+                .setMessage("Send your Doodle in a Messenger conversation, or share through other applications")
+                .setOption1("Messenger", R.drawable.messenger_button_blue_bg_round, new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         context.onShareClicked(true);
                     }
-                });
-                findViewById(R.id.shareButton).setOnClickListener(new View.OnClickListener() {
+                })
+                .setOption2("Default", R.drawable.ic_share, new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         context.onShareClicked(false);
                     }
-                });
+                })
+                .create();
+    }
 
-                // Initialize AdMob SDK
-                MobileAds.initialize(this.getContext(), "ca-app-pub-6293532072634065~6156179621");
 
-                // Load BANNER Ad
-                final AdView mAdView = this.findViewById(R.id.adView);
-                mAdView.setAdListener(new AdListener() {
+    public Dialog supportDialog(final Context context) {
+        return new OptionsDialog.Builder(context)
+                .setIcon(R.drawable.ic_action_review)
+                .setTitle("Support Us")
+                .setMessage("Doodle is a free-of-charge, open-source project. Our team is hard at work to bring you the best product. Minimal ads are the only income source from this app. You can support Doodle by reviewing it on Play Store, contributing to its source, or donating a one-time amount to remove all ads from the app.")
+                .setOption1("Review", R.drawable.ic_support_review, new View.OnClickListener() {
                     @Override
-                    public void onAdLoaded() {
-                        super.onAdLoaded();
-                        mAdView.setVisibility(View.VISIBLE);
+                    public void onClick(View view) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=sfllhkhan95.doodle"));
+                        context.startActivity(browserIntent);
                     }
-
+                })
+                .setOption2("Contribute", R.drawable.ic_support_github, new View.OnClickListener() {
                     @Override
-                    public void onAdFailedToLoad(int i) {
-                        super.onAdFailedToLoad(i);
+                    public void onClick(View view) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/sfllhkhan95/paint"));
+                        context.startActivity(browserIntent);
                     }
-                });
-                AdRequest adRequest = new AdRequest.Builder().build();
-                mAdView.loadAd(adRequest);
-            }
-        };
+                })
+                .setOption3("Donate", R.drawable.ic_support_donate, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(context, "One-time in-app purchase to remove ads", Toast.LENGTH_SHORT).show();
+                        // TODO: Implement in-app purchases
+                    }
+                })
+                .setLabelsEnabled(true)
+                .create();
     }
 
     public Dialog saveAsConfirmationDialog(Context context) {
