0e27d81707e3fca9f2fefa24ee28088f76e7d797
Saif Khan <saifkhichi96@gmail.com>
2018-06-07 07:46:37 +0500

    Updated UI of FAQsActivity to match theme

--- app/src/main/java/sfllhkhan95/doodle/FAQsActivity.java
+++ app/src/main/java/sfllhkhan95/doodle/FAQsActivity.java
@@ -2,15 +2,17 @@ package sfllhkhan95.doodle;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import us.feras.mdv.MarkdownView;
 
 /**
- *
+ * Frequently-Asked Questions (FAQs) about the Doodle application are read in from an markdown
+ * file and displayed in this activity.
  *
  * @author saifkhichi96
- * @version 1.0
- * created on 23/10/2017 2:26 AM
+ * @version 1.0.1
+ * @since 3.2.0 25/04/2018 10:24 AM
  */
 public class FAQsActivity extends AppCompatActivity {
 
@@ -19,8 +21,19 @@ public class FAQsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faqs);
 
-        MarkdownView markdownView = (MarkdownView) findViewById(R.id.markdownView);
-        markdownView.loadMarkdownFile("file:///android_asset/FAQ.md");
+        // Set up action bar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Doodle FAQs");
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Load and display FAQs document
+        MarkdownView markdownView = findViewById(R.id.markdownView);
+        markdownView.loadMarkdownFile(
+                getResources().getString(R.string.url_faqs),
+                getResources().getString(R.string.url_faqs_css)
+        );
     }
 
 }
