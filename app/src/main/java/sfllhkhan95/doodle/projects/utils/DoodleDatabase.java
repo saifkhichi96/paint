24d11b0377583dc28b3b13c7ab9d805af0f0b37e
Muhammad Saifullah Khan <sfllhkhan95@live.com>
2018-06-15 12:38:02 +0500

    Default save path of the projects updated. They no longer appear in Gallery.

--- app/src/main/java/sfllhkhan95/doodle/projects/utils/DoodleDatabase.java
+++ app/src/main/java/sfllhkhan95/doodle/projects/utils/DoodleDatabase.java
@@ -16,25 +16,77 @@ import java.util.List;
 import java.util.Locale;
 
 /**
+ * This is the project database. DoodleDatabase is responsible for managing all the project
+ * files, including saving, opening and deleting them.
+ *
  * @author saifkhichi96
+ * @version 1.3.0
+ * @since 1.0.0
  */
 public class DoodleDatabase {
 
-    private final static String PREFIX_V1 = "DOODLE_";
-    private final static String PREFIX = "DOODLEv2_";
-    private final static String DIR = "Pictures/Doodles/";
     private final static String EXT = ".jpg";
 
-    private final static File rootDir;
+    private final static String PREFIX = "DOODLEv2_";
+    private final static String DIR = "Android/data/sfllhkhan95.doodle/files/Projects/";
     private final static String rootDirPath;
+    private final static File rootDir;
+
+    private final static String PREFIX_LEGACY = "DOODLE_";
+    private final static String DIR_LEGACY = "Pictures/Doodles/";
+    private final static String legacyRootDirPath;
+    private final static File legacyRootDir;
 
     static {
+        legacyRootDirPath = Environment.getExternalStorageDirectory().toString() + "/" + DIR_LEGACY;
+        legacyRootDir = new File(legacyRootDirPath);
+        legacyRootDir.mkdirs();
+
         rootDirPath = Environment.getExternalStorageDirectory().toString() + "/" + DIR;
         rootDir = new File(rootDirPath);
         rootDir.mkdirs();
     }
 
-    private static String upgradeToV2(String oldName) {
+    /**
+     * Relocates a project to new root directory.
+     *
+     * @param projectName name of the project to be relocated
+     * @since 3.4.3
+     */
+    private static void migrateDoodle(String projectName) {
+        Bitmap doodle = DoodleFactory.loadFromPath(legacyRootDirPath + projectName);
+        saveDoodle(doodle, projectName);
+        removeDoodle(legacyRootDir, projectName);
+    }
+
+    /**
+     * Relocates all projects to the updated root directory.
+     *
+     * @since 3.4.3
+     */
+    private static void migrateLegacyDoodles() {
+        String[] list = legacyRootDir.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return (name.startsWith(PREFIX) || name.startsWith(PREFIX_LEGACY)) && name.endsWith(EXT);
+            }
+        });
+        if (list != null) {
+            for (String name : list) {
+                migrateDoodle(name);
+            }
+        }
+        legacyRootDir.delete();
+    }
+
+    /**
+     * Renames projects to reflect updated naming syntax.
+     *
+     * @param oldName old name of the project
+     * @return new name of the project
+     * @since 2.1.0
+     */
+    private static String upgradeLegacyNames(String oldName) {
         String date = oldName.split("_")[1];
         String time = oldName.split("_")[2];
 
@@ -52,30 +104,36 @@ public class DoodleDatabase {
         return newName;
     }
 
-    public static void saveDoodle(final Bitmap doodle) {
-        // Create file for storage with CURRENT_TIMESTAMP as name
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US);
-        String timestamp = simpleDateFormat.format(new Date());
-        String filename = PREFIX + timestamp + EXT;
-
-        saveDoodle(doodle, filename);
-    }
-
+    /**
+     * Opens a project as a Bitmap image of specified size.
+     *
+     * @param projectName name of the project to open
+     * @param wd          width of the opened image
+     * @param ht          height of the opened image
+     * @return the Bitmap image of the project, or null
+     */
     @Nullable
-    public static Bitmap loadDoodle(String filename, int wd, int ht) {
-        File file = new File(rootDir, filename);
+    public static Bitmap loadDoodle(String projectName, int wd, int ht) {
+        File file = new File(rootDir, projectName);
         if (file.exists()) {
-            return DoodleFactory.loadFromPath(rootDirPath + filename, wd, ht);
+            return DoodleFactory.loadFromPath(rootDirPath + projectName, wd, ht);
         } else {
             return null;
         }
     }
 
+    /**
+     * Lists all the projects in the database.
+     *
+     * @return a list containing names of all the projects, or null
+     */
+    @Nullable
     public static String[] listDoodles() {
+        migrateLegacyDoodles();
         String[] list = rootDir.list(new FilenameFilter() {
             @Override
             public boolean accept(File dir, String name) {
-                return (name.startsWith(PREFIX) || name.startsWith(PREFIX_V1)) && name.endsWith(EXT);
+                return (name.startsWith(PREFIX) || name.startsWith(PREFIX_LEGACY)) && name.endsWith(EXT);
             }
         });
         if (list == null) {
@@ -86,8 +144,8 @@ public class DoodleDatabase {
 
         for (String name : list) {
             // If file is from an older version, upgrade filename
-            if (name.startsWith(PREFIX_V1)) {
-                name = upgradeToV2(name);
+            if (name.startsWith(PREFIX_LEGACY)) {
+                name = upgradeLegacyNames(name);
             }
 
             newList.add(name);
@@ -98,12 +156,32 @@ public class DoodleDatabase {
         return list;
     }
 
-    public static void saveDoodle(Bitmap doodle, String filename) {
-        File file = new File(rootDir, filename);
+    /**
+     * Saves a new project to the database.
+     *
+     * @param projectData content of the new project
+     */
+    public static void saveDoodle(final Bitmap projectData) {
+        // Create file for storage with CURRENT_TIMESTAMP as name
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US);
+        String timestamp = simpleDateFormat.format(new Date());
+        String filename = PREFIX + timestamp + EXT;
+
+        saveDoodle(projectData, filename);
+    }
+
+    /**
+     * Saves a new project to the database.
+     *
+     * @param projectData content of the new project
+     * @param projectName name of the new project
+     */
+    public static void saveDoodle(Bitmap projectData, String projectName) {
+        File file = new File(rootDir, projectName);
         if (file.exists()) file.delete();
         try {
             FileOutputStream out = new FileOutputStream(file);
-            doodle.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            projectData.compress(Bitmap.CompressFormat.JPEG, 90, out);
             out.flush();
             out.close();
         } catch (Exception e) {
@@ -111,15 +189,37 @@ public class DoodleDatabase {
         }
     }
 
-    public static boolean contains(String filename) {
-        File file = new File(rootDir, filename);
+    /**
+     * Checks if a project file exists in the database.
+     *
+     * @param projectName name of the project to check
+     * @return true if the project is found, false otherwise
+     */
+    public static boolean contains(String projectName) {
+        File file = new File(rootDir, projectName);
         return (file.exists());
     }
 
-    public static File removeDoodle(String filename) {
-        File file = new File(rootDir, filename);
-        if (file.exists()) file.delete();
-        return file;
+    /**
+     * Deletes a project.
+     *
+     * @param projectName name of the project to be deleted
+     * @return true if the project is successfully deleted, false otherwise
+     */
+    public static boolean removeDoodle(String projectName) {
+        return removeDoodle(rootDir, projectName);
+    }
+
+    /**
+     * Deletes a project.
+     *
+     * @param rootDir     root directory where this project is located
+     * @param projectName name of the project to be deleted
+     * @return true if the project is successfully deleted, false otherwise
+     */
+    private static boolean removeDoodle(File rootDir, String projectName) {
+        File file = new File(rootDir, projectName);
+        return file.exists() && file.delete();
     }
 
 }
